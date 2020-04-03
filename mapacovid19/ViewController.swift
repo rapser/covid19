aebd8422ba77362e7544f414643dd74a3404a7ed
Miguel Tomairo <matomairo@gmail.com>
2020-04-03 17:34:21 -0500

    Initial Commit

---
+++ mapacovid19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mapacovid19
+//
+//  Created by miguel tomairo on 4/3/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
