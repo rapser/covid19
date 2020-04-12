47939e6f4f822148e823cf284e57f2b37cdc4029
Miguel Tomairo <matomairo@gmail.com>
2020-04-12 16:56:13 -0500

    rename project

---
+++ chasqui/Models/Covid.swift
@@ -0,0 +1,15 @@
+//
+//  Covid.swift
+//  chasqui
+//
+//  Created by miguel tomairo on 4/3/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import Foundation
+
+struct Covid: Codable {
+    var title, description: String
+    var lat, lon: Double
+    var ubigeo: Int
+}
